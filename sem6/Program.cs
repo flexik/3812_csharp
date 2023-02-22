65cd6899d67133106058547846cbbc04d3dcb923
Artyome <flex785834@yandex.ru>
2023-02-22 18:29:59 +0300

    add seminar 6

--- sem6/Program.cs
+++ sem6/Program.cs
@@ -1,5 +1,86 @@
-﻿//Напишите программу, которая перевернёт одномерный массив 
-//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+﻿// Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
 
-Console.Clear();
+// Console.Clear();
 
+// int[] Array1 = GetArray(10, 0, 10);
+// Console.WriteLine(String.Join(", ", Array1));
+
+// int[] GetArray(int size, int min, int max)    // метод возвращающий заданный массив
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = new Random().Next(min, max + 1);
+//     }
+//     return result;
+// }
+
+// int[] ArrayConverse(int[] arr){              // метод возвращающий перевернутый массив
+//     int[] result = new int[arr.Length];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         result[i] = arr[arr.Length - i - 1];
+//     }
+//     return result;
+// }
+
+// int[] conversedArray = ArrayConverse(Array1);
+// Console.WriteLine(String.Join(", ", conversedArray));
+
+
+/*______________________________________________________________________*/
+
+// **Задача 40:**Напишите программу,
+// которая принимает на вход три числа и проверяет, может ли
+// существовать треугольник с сторонами такой длины.
+
+
+// ❗ **Теорема о неравенстве треугольника:
+// **каждая сторона треугольника меньше суммы двух других сторон.
+// Console.WriteLine("Введите стороны треугольника через пробел: ");
+
+// string[] st = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+// int a = Int32.Parse(st[0]);
+// int b = Int32.Parse(st[1]);
+// int c = Int32.Parse(st[2]);
+
+// bool IsTriangle(int a, int b, int c)                 // ф-ия возвращающая true или false
+// {
+//     return (((a + b) > c) && ((a + c) > b) && ((b + c) > a));
+// }
+
+// if (IsTriangle(a, b, c))       // если функция вернет true от наших 3 чисел, то - да, если false то - нет 
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+//____________________________________________________________//
+
+// **Задача 42:**Напишите программу,
+// которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+//Универсальный перевод в двоичную систему из любой
+// string DecToNum(int decNumber, int otherSystem)
+// {
+// string res = "";
+// string nums = "0123456789ABCDEF";
+// while (decNumber > 0)
+// {
+// int ost = decNumber / otherSystem;
+// res = nums[decNumber - otherSystem * ost] + res;
+// decNumber /= otherSystem;
+// }
+// return res;
+// }
+
+/*Задача 45: Напишите программу,
+которая будет создавать копию заданного массива с помощью поэлементного копирования.*/
---
+++ sem7/Program.cs
@@ -0,0 +1,84 @@
+﻿// **Задача 48:** Задайте
+// двумерный массив размера m на n, каждый элемент
+//  в массиве находится по формуле: A[i][j] = i + j.
+//Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Write($"{inArray[i,j]} ");
+//         }
+//         WriteLine();
+//     }
+// }
+
+// **Задача 49:** Задайте двумерный массив.
+//  Найдите элементы, у которых оба индекса чётные,
+//   и замените эти элементы на их квадраты.
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 2 3 4
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 **81** 2 **9**
+// 8 4 2 4
+// 1 **4** 3 **16**
+
+// void PrintNewArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             if (i % 2 != 0 && j % 2 != 0) Console.Write($"{inArray[i, j] * inArray[i, j]} ");
+//             else Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// Задача 51: Задайте двумерный массив.
+// Найдите сумму элементов, находящихся
+//  на главной диагонали (с индексами (0,0); (1; 1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int GetSum(int[,] inArray)
+{
+    int result = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == j)
+                result += inArray[i, j];
+        }
+    }
+    return result;
+}
